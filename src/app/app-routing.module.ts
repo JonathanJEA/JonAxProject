27b89e8ed1130f604abbec2db852546454c181f3
Jonathan Jesús Estrada Arenas <jonathan.estrada.arenas@hotmail.com>
2019-08-10 04:42:30 -0500

    start develoment application front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/*** Components Route ***/
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CatalogsComponent } from './catalogs/catalogs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/sign-in', component: SignInComponent },
+  { path: '/sign-up', component: SignUpComponent },
+  { path: '/application/home', component: HomeComponent },
+  { path: '/application/settings', component: SettingsComponent },
+  { path: '/application/shopping-cart', component: ShoppingCartComponent },
+  { path: '/application/catalogs', component: CatalogsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CatalogsComponent } from './catalogs/catalogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent,
+    SettingsComponent,
+    ShoppingCartComponent,
+    CatalogsComponent
   ],
   imports: [
     BrowserModule,
